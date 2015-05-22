921b4d648e13a4c4b449923bbbc09e633731fb86
Austin <austineady@github.com>
2015-05-22 13:40:16 -0400

    links working

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ function that will receive the response.
 var url = "https://api.etsy.com/v2/listings/active.js?api_key=kz94tq0fg2i6mkj1gk0g2glg&keywords=skyrim&includes=Images,Shop";
 fetchJSONP(url, currentArrayItem);
 
+var combinedArrays = [];
+
 function fetchJSONP(url, callback) {
     var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
     var script = document.createElement('script');
@@ -25,9 +27,25 @@ function fetchJSONP(url, callback) {
 function currentArrayItem(data) {
   for(var i = 0; i < data.results.length; i++) {
     var item = data.results[i];
+    var category = data.results[i].category_path;
     changeVariables(item);
-    console.log(data);
+    combineArrays(category);
 }
+}
+
+function determineCategories(array) {
+  combinedArrays.forEach(function(value, index) {
+    combinedArrays.shift()
+  })
+}
+
+function combineArrays(category) {
+  category.forEach(function(value, index) {
+    if (combinedArrays.indexOf(value) === -1) {
+      combinedArrays.push(value);
+      changeCategory(value);
+    }
+  })
 }
 
 function changeVariables(item) {
@@ -35,8 +53,22 @@ function changeVariables(item) {
   var title = String(item.title);
   var seller = item.Shop.login_name;
   var price = item.price;
-  var context = {inputimage: image, inputtitle: title, inputseller: seller, inputprice: price};
+  var itemsite = item.url;
+  var sellersite = item.Shop.url;
+  var context = {
+    inputimage: image,
+    inputtitle: title,
+    inputseller: seller,
+    inputprice: price,
+    inputitemsite: itemsite,
+    inputsite: sellersite
+  };
   logResults(context);
+}
+
+function changeCategory(value) {
+  var categoryValue = {categoryname: value};
+  displayCategories(categoryValue);
 }
 
 function logResults(context) {
@@ -44,8 +76,16 @@ function logResults(context) {
   var template = Handlebars.compile(source);
   var output = template(context);
   var templateHtml = template({});
-  var findDiv = document.querySelector('.middlecontainer');
+  var findDiv = document.querySelector('.sortbar');
   findDiv.insertAdjacentHTML("beforeend", output);
+}
+
+function displayCategories(categoryValue) {
+  var source = document.querySelector("#category-template").innerHTML;
+  var template = Handlebars.compile(source);
+  var output = template(categoryValue);
+  var findDisplayLi = document.querySelector('.head');
+  findDisplayLi.insertAdjacentHTML("beforeend", output);
 }
 
 /*Then you can call the function like this,
